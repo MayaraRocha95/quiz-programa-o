47a2e20916a686a469bd5be9cc48659258a76562
Mayara Rocha <mayaraaraujorocha@hotmail.com>
2023-03-23 18:53:43 -0300

    mudança de estado

--- quiz/src/context/quiz.jsx
+++ quiz/src/context/quiz.jsx
@@ -7,15 +7,18 @@ const STAGES = ["Start", "Playing", "End"]
 
 const initialState = {
     gameStage: STAGES[0],
-    questions
-}
+    questions,
+};
 
 const quizReducer = (state, action) => {
 console.log (state,action);
 
     switch(action.type){
         case "CHANGE_STATE":
-         return state;
+         return {
+            ...state,
+            gameStage: STAGES[1],
+         };
 
 
        default:

cd0bc19fdfb44325bcab33d698857dd832f6a30a
Mayara Rocha <mayaraaraujorocha@hotmail.com>
2023-03-23 17:56:03 -0300

    criando o componente welcome

--- quiz/src/components/Welcome.jsx
+++ quiz/src/components/Welcome.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import Quiz from '../img/quiz.svg';
+
+import './Welcome.css'
 
 const Welcome = () => {
   return (
-    <div>
+    <div id='welcome'>
+      <h2>Seja bem-vindo</h2>
+      <p>Clique no botão abaixo para começar:</p>
+      <button>Iniciar</button>
+      <img src={Quiz} alt='Inicio do Quiz'/>
       
     </div>
   )

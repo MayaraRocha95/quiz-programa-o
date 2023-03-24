1be2c618c11ecea788760618db9e8bb809f5c557
Mayara Rocha <mayaraaraujorocha@hotmail.com>
2023-03-24 14:55:48 -0300

    adicionando Game Over

--- quiz/src/App.jsx
+++ quiz/src/App.jsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { QuizContext } from './context/quiz';
 import Welcome from './components/Welcome';
 import Question from './components/Question';
+import GameOver from './components/GameOver';
 import './App.css';
 import { useEffect} from 'react';
 
@@ -16,6 +17,7 @@ function App() {
       <h1>Quiz de Programação</h1>
       {quizState.gameStage === "Start" && <Welcome />}
       {quizState.gameStage === "Playing" && <Question />}
+      {quizState.gameStage === "End" && <GameOver />}
     </div>
   )
 }

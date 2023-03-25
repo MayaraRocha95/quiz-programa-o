722aa8acd4c742f648aef0bde89e53db567a3c47
Mayara Rocha <mayaraaraujorocha@hotmail.com>
2023-03-25 17:34:34 -0300

    finalizando o projeto inicial

--- quiz/src/components/Option.jsx
+++ quiz/src/components/Option.jsx
@@ -1,16 +1,26 @@
-import './Option.css';
+import { useContext } from "react";
 
-import { useContext } from 'react';
-import { QuizContext } from '../context/quiz';
+import { QuizContext } from "../context/quiz";
 
-function Option({ option, selectOption, answer }) {
-    const [quizState, dispatch] =  useContext(QuizContext);
+import "./Option.css";
+
+const Option = ({ option, selectOption, answer, hide }) => {
+  const [quizState, dispatch] = useContext(QuizContext);
 
   return (
-    <div className='option' onClick={() => selectOption()}>
-      <p>{option}</p> 
+    <div
+      onClick={() => selectOption()}
+      className={`
+      option
+        ${quizState.answerSelected && option === answer ? "correct" : ""} ${
+        quizState.answerSelected && option !== answer ? "wrong" : ""
+      }
+        ${hide ? "hide" : ""}
+        `}
+    >
+      <p>{option}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Option
+export default Option;
--- quiz/src/context/quiz.jsx
+++ quiz/src/context/quiz.jsx
@@ -14,58 +14,54 @@ const initialState = {
 };
 
 const quizReducer = (state, action) => {
-
-
     switch(action.type){
-        case "CHANGE_STATE":
-         return {
-            ...state,
-            gameStage: STAGES[1],
-         };
-
-        case "REORDER_QUESTIONS":
-           const reorderQuestions = questions.sort(() => {
-            return Math.random() - 0.5;
-           });
-            return{
-                ...state,
-                questions:reorderQuestions,
-            };
-        case "CHANGE_QUESTION":
-            const nexQuestion = state.currentQuestion + 1;
-            let endGame = false;
-
-            if (!questions[nexQuestion]){
-                endGame = true;
-            }
-
-            return {
-                ...state,
-                currentQuestion: nexQuestion,
-                gameStage: endGame ? STAGES[2] : state.gameStage,
-            }
-        case "NEW_GAME": 
-         return initialState;
-        
-        case "CHECK_ANSWER":
-            if(state.answerSelected) return state;
-         const answer = action.payload.answer 
-         const option = action.payload.option 
-         let correctAnswer = 0
-
-         if (answer === option) correctAnswer = 1;
-
-         return {
-            ...state,
-            score: state.score = correctAnswer,
-            answerSelected: option,
-         }
-         
-       default:
+      case "CHANGE_STATE":
+        return {
+          ...state,
+          gameStage: STAGES[1],
+        };
+      case "REORDER_QUESTIONS":
+        const reorderQuestions = state.questions.sort(() => {
+          return Math.random() - 0.5;
+        });
+        return {
+          ...state,
+          questions: reorderQuestions,
+        };
+      case "CHANGE_QUESTION":
+        const nexQuestion = state.currentQuestion + 1;
+        let endGame = false;
+  
+        if (!state.questions[nexQuestion]){
+          endGame = true;
+        }
+  
+        return {
+          ...state,
+          currentQuestion: nexQuestion,
+          gameStage: endGame ? STAGES[2] : state.gameStage,
+          answerSelected: false,
+        };
+      case "NEW_GAME":
+        return initialState;
+      case "CHECK_ANSWER":
+        if(state.answerSelected) return state;
+        const answer = action.payload.answer;
+        const option = action.payload.option;
+        let correctAnswer = 0;
+  
+        if (answer === option) correctAnswer = 1;
+  
+        return {
+          ...state,
+          score: state.score + correctAnswer,
+          answerSelected: option,
+        };
+      default:
         return state;
     }
-};
-
+  };
+  
 
 export const QuizContext = createContext();
 
